149f348cd51830bcfecf511ea2532a378beb51a5
ShadowJod <nikhilmishra0187@gmail.com>
2025-12-21 23:33:15 +0530

    hero section ready

---
+++ class1/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Nav from './Components/Nav.jsx'
+import './index.css'
+import Hero from './Components/Hero.jsx'
+
+const App = () => {
+  return (
+    <div className=''>
+      <Nav/>
+      <Hero/>
+
+    </div>
+  )
+}
+
+export default App
