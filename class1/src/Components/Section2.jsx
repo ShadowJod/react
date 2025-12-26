7576510b047645eb265a43d55f6dd8997463080c
ShadowJod <nikhilmishra0187@gmail.com>
2025-12-26 21:15:33 +0530

    addded

--- class1/src/Components/Section2.jsx
+++ class1/src/Components/Section2.jsx
@@ -4,10 +4,10 @@ import img3 from '../assets/img3.webp'
 const Section2 = () => {
   return (
     <div className='flex bg-[#121212] text-[#F4F2ED] px-3'>
-        <div></div>
+        <div>h1</div>
         <div></div>
     </div>
   )
 }
 
-export default Section2
+export default Section2 
